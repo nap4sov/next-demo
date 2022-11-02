8c66311822328f3f9f648115e8474c494882db91
Roman <g4vr0m@gmail.com>
2022-11-02 10:10:18 +0200

    fix .env vars

--- next.config.js
+++ next.config.js
@@ -16,10 +16,10 @@ module.exports = {
     ],
     domains: ['avatars.githubusercontent.com'],
   },
-  env: {
-    GITHUB_ID: 'e148646cdc99180a50cf',
-    GITHUB_SECRET: 'bff44d84d74d5d6b4ffe230a01edd8d7255c70e7',
-    GOOGLE_ID: '69iea4vjeek6kf1jh35b2562p9nd05h7.apps.googleusercontent.com',
-    GOOGLE_SECRET: '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',
-  },
+  // env: {
+  //   GITHUB_ID: 'e148646cdc99180a50cf',
+  //   GITHUB_SECRET: 'bff44d84d74d5d6b4ffe230a01edd8d7255c70e7',
+  //   GOOGLE_ID: '69iea4vjeek6kf1jh35b2562p9nd05h7.apps.googleusercontent.com',
+  //   GOOGLE_SECRET: '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',
+  // },
 };
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,17 +2,15 @@ import NextAuth from 'next-auth/next';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
-const { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET } = process.env;
-
 export default NextAuth({
   providers: [
     GitHubProvider({
-      clientId: GITHUB_ID as string,
-      clientSecret: GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     GoogleProvider({
-      clientId: GOOGLE_ID as string,
-      clientSecret: GOOGLE_SECRET as string,
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
 });
