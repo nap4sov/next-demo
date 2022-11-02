43267dcbc6c09817b09b6a19445de6df6a81df89
Roman <g4vr0m@gmail.com>
2022-11-02 09:36:08 +0200

    fix deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,13 @@
-import { useEffect, useState } from 'react';
 import { Header } from '../components/Header/Header';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import { cookies, getUserInfo } from '../api';
-import { IUser } from '../interfaces/user';
+
 import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [user, setUser] = useState<IUser>();
-
-  useEffect(() => {
-    (async () => {
-      const token = cookies.get('token');
-      if (!token) return;
-      const userInfo = await getUserInfo(token);
-      setUser(userInfo);
-    })();
-  }, [Component]);
-
   return (
     <SessionProvider session={pageProps.session}>
-      <Header user={user} />
+      <Header />
       <main className="main">
         <Component {...pageProps} />
       </main>
