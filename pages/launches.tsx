9a94609ba3a7c87039b7d1cf771fefdf7630ddc6
Roman <g4vr0m@gmail.com>
2022-11-03 22:53:38 +0200

    add meta & build cache

--- pages/launches.tsx
+++ pages/launches.tsx
@@ -1,6 +1,7 @@
 // import { client } from '../graphql/apolloClient';
 // import { GET_LAUNCHES } from '../graphql/queries';
 // import { ILaunchesQuery } from '../graphql/interfaces';
+import { Meta } from '../components/Meta';
 import { LaunchesList } from '../components/Launches/LaunchesList';
 import { CanvasRoot } from '../components/Canvas';
 import { Stars } from '@react-three/drei';
@@ -8,6 +9,7 @@ import { Stars } from '@react-three/drei';
 const Launches: React.FC = () => {
   return (
     <>
+      <Meta title="SpaceX Launches" description="SpaceX past launches" />
       <LaunchesList />
       <CanvasRoot className="dark">
         <Stars />
