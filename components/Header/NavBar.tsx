62f056e8769243a7dccef10f08c1897939c1978b
Roman <g4vr0m@gmail.com>
2022-11-03 22:37:20 +0200

    ui fixes

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { NAV_ROUTES } from '../../constants/navRoutes';
 import styles from '../../styles/Header.module.scss';
 
 export const NavBar: React.FC = () => {
+  const router = useRouter();
+
   return (
     <nav className={styles.nav}>
-      {NAV_ROUTES.map(route => (
-        <Link key={route.label} href={route.route} className={styles.link}>
-          {route.label}
+      {NAV_ROUTES.map(({ label, route }) => (
+        <Link
+          key={label}
+          href={route}
+          className={router.asPath === route ? styles.activeLink : styles.link}
+        >
+          {label}
         </Link>
       ))}
     </nav>
